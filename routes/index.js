c923791849e99597cce995f5ffd315cbff77ab68
CarlosFabianAguirre <carlosfabianaguirre@hotmail.com>
2022-06-16 20:19:12 -0300

    initial comit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'soy un capo' });
+});
+
+module.exports = router;
