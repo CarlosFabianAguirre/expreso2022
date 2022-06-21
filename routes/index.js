6daa7b1398d7dd6e4f3902ad74fb489bc0d0116b
CarlosFabianAguirre <carlosfabianaguirre@hotmail.com>
2022-06-21 20:33:23 -0300

    metodo post

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'soy un capo' });
+  res.render('index', { title: 'HARRY POTTER' });
 });
 /*get nosotros page */
+router.get("/buscar",(req, res)=>{
+  //guardar en la variable lo que escribio el usuario en la consola
+  let{ termino} =re.query;
+  //mostrar en la terminal
+  console.log(req.query.termino);
+  res.send("estas en buscar")
+});
+router.post("/agregar-libro",(req, res)=>{
+  console.log(req.body);
+  let {titulo, autor, precio} = req.body;
+  res.send(`agregaron ${titulo } ${autor} ${precio}`
+  )
+});
+
+router.get("/agregar",(req, res)=>{
+  res.render("pages/agregar")
+});
+
 router.get("/nosotros",(req, res)=>{
   res.render("pages/nosotros")
 });
 router.get("/contacto",(req, res)=>{
-  res.render("pages/contacto")
+  res.send("pages/contacto")
 });
 module.exports = router;
